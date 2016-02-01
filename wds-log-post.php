fb52b6ff31c4722fec5095ea8e9edc19070bde67
Zach Owen <phatsk@gmail.com>
2016-01-31 20:00:58 -0500

    Enqueue log scripts in main class. Add key as accessible property

--- wds-log-post.php
+++ wds-log-post.php
@@ -227,10 +227,7 @@ class WDS_Log_Post {
 	public function register_enqueue_scripts_styles() {
 		$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 		wp_register_style( $this->key . '_admin_css', self::url( "assets/css/wds-log-post{$min}.css" ), $this->version );
-
-		// Enqueue progress bar.
-		wp_enqueue_script( 'jquery-ui-progressbar' );
-		wp_enqueue_script( 'heartbeat' );
+		wp_register_script( $this->key . '_admin_js', self::url( 'assets/js/main.min.js' ), array( 'jquery' ), self::VERSION );
 	}
 
 	public function maybe_enqueue_scripts_styles() {
@@ -241,6 +238,11 @@ class WDS_Log_Post {
 		}
 
 		wp_enqueue_style( $this->key . '_admin_css' );
+
+		// Enqueue progress bar.
+		wp_enqueue_script( 'jquery-ui-progressbar' );
+		wp_enqueue_script( 'heartbeat' );
+		wp_enqueue_script( $this->key . '_admin_js' );
 	}
 
 	/**
@@ -294,6 +296,7 @@ class WDS_Log_Post {
 			case 'basename':
 			case 'url':
 			case 'path':
+			case 'key':
 				return $this->$field;
 			default:
 				throw new Exception( 'Invalid '. __CLASS__ .' property: ' . $field );
