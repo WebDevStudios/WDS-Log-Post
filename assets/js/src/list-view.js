f3685027025a5dacd6710c13097c136c3962fc8b
Zach Owen <phatsk@gmail.com>
2016-01-31 19:59:02 -0500

    List view JS enabled

--- assets/js/src/list-view.js
+++ assets/js/src/list-view.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function($){
 	// Remove the bulk action selector.
-	//$('#bulk-action-selector-top').find('option[value="edit"]').remove();
+	$('#bulk-action-selector-top').find('option[value="edit"]').remove();
 });
